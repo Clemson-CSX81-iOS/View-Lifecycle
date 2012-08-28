103981df3512048ff5abbc7be7a8cdee5305f9a4
DJ Edmonson <dedmons@g.clemson.edu>
2012-08-28 11:16:01 -0400

    Cleanup and fixed a leak

--- View Lifecycle/StartViewController.m
+++ View Lifecycle/StartViewController.m
@@ -16,12 +16,12 @@
  */
 
 - (IBAction)animated:(id)sender {
-    [self.navigationController pushViewController:[self.storyboard instantiateViewControllerWithIdentifier:@"vc1"]
+    [self.navigationController pushViewController:[[self.storyboard instantiateViewControllerWithIdentifier:@"vc1"]autorelease]
                                          animated:YES];
 }
 
 - (IBAction)notAnimated:(id)sender {
-    [self.navigationController pushViewController:[self.storyboard instantiateViewControllerWithIdentifier:@"vc1"]
+    [self.navigationController pushViewController:[[self.storyboard instantiateViewControllerWithIdentifier:@"vc1"]autorelease]
                                          animated:NO];
 }
 
